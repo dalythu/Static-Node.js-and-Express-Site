dc3f9d516725d93d2b2c3b7673c473b29c5ef96e
Thurston Daly <dalythu@gmail.com>
2021-08-29 20:55:24 -0400

    Update App.js and functional pug files

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { data } = require('./data.json');
+const { projects } = require('./data.json');
 const path = require("path");
 
 const app = express();
@@ -8,10 +8,12 @@ app.set('view engine', 'pug');
 
 app.use('/static', express.static('public'));
 
+//Routes
 app.get('/', (req, res) => {
-    res.render('index', { data });
+    res.render('index', { projects });
 });
 
+//
 app.get('/about', (req, res) => {
     res.render('./about');
 });
@@ -19,10 +21,37 @@ app.get('/about', (req, res) => {
 //
 app.get("/projects/:id", (req, res) => {
     const projectId = req.params.id;
-    res.render('project', { data: projects[projectId]});
+    res.render('project', { project: projects[projectId] });
 });
+
+app.get('/projects', (req, res) => {
+    res.redirect('/projects/0');
+});
+
+//404 error
+// app.use((req, res, next) => {
+//     const err = new Error('Page not found');
+//     err.status = 400;
+//     next(err);
+// });
+
+// //Global Error
+// app.use((err, req, res, next) => {
+//     if (err.status === 400) {
+//         res.locals.error = err;
+//         err.message = 'Page not found';
+//         res.status(err.status);
+//         res.render('error');
+//         console.log(err.message, err.status);
+//     } else {
+//         res.locals.error = err;
+//         err.message = 'It looks like something went wrong on the server.';
+//         err.status = 500;
+//         render('error');
+//         console.log(err.message, err.status);
+//     }
+//   });
 
 app.listen(3000, () => {
     console.log('The application is running on localhost:3000!')
 });
-
