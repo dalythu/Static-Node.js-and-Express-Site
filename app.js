6a97095734ba9fea847ee864bfce81bd68f3b902
Thurston Daly <dalythu@gmail.com>
2021-08-30 00:31:08 -0400

    updated error handling and images

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const path = require("path");
 
 const app = express();
 
+//view engine is set to pug
 app.set('view engine', 'pug');
 
+//static files and path
 app.use('/static', express.static('public'));
 
 //Routes
@@ -13,12 +15,10 @@ app.get('/', (req, res) => {
     res.render('index', { projects });
 });
 
-//
 app.get('/about', (req, res) => {
     res.render('./about');
 });
 
-//
 app.get("/projects/:id", (req, res) => {
     const projectId = req.params.id;
     res.render('project', { project: projects[projectId] });
@@ -28,30 +28,28 @@ app.get('/projects', (req, res) => {
     res.redirect('/projects/0');
 });
 
-//404 error
-// app.use((req, res, next) => {
-//     const err = new Error('Page not found');
-//     err.status = 400;
-//     next(err);
-// });
 
-// //Global Error
-// app.use((err, req, res, next) => {
-//     if (err.status === 400) {
-//         res.locals.error = err;
-//         err.message = 'Page not found';
-//         res.status(err.status);
-//         res.render('error');
-//         console.log(err.message, err.status);
-//     } else {
-//         res.locals.error = err;
-//         err.message = 'It looks like something went wrong on the server.';
-//         err.status = 500;
-//         render('error');
-//         console.log(err.message, err.status);
-//     }
-//   });
+//404 error
+app.use((req, res, next) => {
+    const err = new Error('Page not found');
+    err.status = 400;
+    next(err);
+});
+
+//Global Error
+app.use((err, req, res, next) => {
+    res.locals.error = err; 
+    if(err.status === 404) {
+        console.log('404 error handler called');
+        err.message = `Page not found`;
+        res.status(404).render('page-not-found', {err});
+    } else {
+        err.message = `It looks like something went wrong on the server.`;
+        console.log(err.message);
+        res.status(err.status || 500).render('error', {err});
+    }
+});
 
 app.listen(3000, () => {
-    console.log('The application is running on localhost:3000!')
+    console.log('The application is running on localhost:3000')
 });
